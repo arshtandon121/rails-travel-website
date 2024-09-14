15cc6e84db2798dba7402469482596dfd0d9858b
arshdeep.tandon <Arshdeep.tandon@planetspark.in>
2024-09-14 22:38:42 +0530

    FIx the Issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  
+  config.hosts.clear 
   config.assets.js_compressor = :terser
   config.assets.css_compressor = :sass
 
